64017201c2a332a36e13244cf115d044cf0b4a5e
Border55-repo <juliannordli@gmail.com>
2026-09-23 13:52:38 +0200

    Refresh cache for KOVA PWA 1.7

--- apps/kova/sw.js
+++ apps/kova/sw.js
@@ -1,4 +1,4 @@
-const CACHE="kova-pwa-v7";
+const CACHE="kova-pwa-v8";
 const SHELL=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg","./firebase-web-config.json"];
 
 self.addEventListener("install",event=>{
