08f2725eaccef692e31b249116236c4e6857b897
Border55-repo <juliannordli@gmail.com>
2026-09-23 18:33:50 +0200

    Refresh cache for completed Mine vakter 2.0

--- apps/kova/sw.js
+++ apps/kova/sw.js
@@ -1,4 +1,4 @@
-const CACHE="kova-pwa-v14";
+const CACHE="kova-pwa-v15";
 const SHELL=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg","./firebase-web-config.json"];
 
 self.addEventListener("install",event=>{
