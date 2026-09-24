7c518a3362a4aaa184a90a466304b9971ead3766
Border55-repo <juliannordli@gmail.com>
2026-09-24 15:00:57 +0200

    Refresh PWA for Android 2.0.1 parity

--- apps/kova/sw.js
+++ apps/kova/sw.js
@@ -1,4 +1,4 @@
-const CACHE="kova-pwa-v27";
+const CACHE="kova-pwa-v28";
 const SHELL=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg","./firebase-web-config.json","./privacy.html"];
 const HISTORY_DB="kova-pwa-history";
 const HISTORY_STORE="notifications";
